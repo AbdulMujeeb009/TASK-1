f55cad24a5cf4055a225210322f9984f7743b69c
AbdulMujeeb009 <shaikabdulmujeeb009@gmail.com>
2025-05-01 17:34:24 +0530

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,48 @@
+
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "#33C3F0",
+          foreground: "#FFFFFF",
+        },
+        secondary: {
+          DEFAULT: "#F97316",
+          foreground: "#FFFFFF",
+        },
+        destructive: {
+          DEFAULT: "#ea384c",
+          foreground: "#FFFFFF",
+        },
+        success: {
+          DEFAULT: "#10B981",
+          foreground: "#FFFFFF",
+        },
+        muted: {
+          DEFAULT: "#403E43",
+          foreground: "#aaadb0",
+        },
+      },
+      fontFamily: {
+        mono: ["JetBrains Mono", "monospace"],
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
